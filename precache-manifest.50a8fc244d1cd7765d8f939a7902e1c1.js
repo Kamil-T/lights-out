8aea19a540b7b2f5f496dacaaaaa08417fd8e506
Kamil T <trzaskomakamil@gmail.com>
2019-09-12 19:27:27 +0200

    Updates

---
+++ precache-manifest.50a8fc244d1cd7765d8f939a7902e1c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b8c933b0f5638b60e09be07c3a07579",
+    "url": "/lights-out/index.html"
+  },
+  {
+    "revision": "ba54e1dd6d596f69a5df",
+    "url": "/lights-out/static/css/main.f9905f5a.chunk.css"
+  },
+  {
+    "revision": "16c701d0ad65f74d534b",
+    "url": "/lights-out/static/js/2.8a404ef1.chunk.js"
+  },
+  {
+    "revision": "ba54e1dd6d596f69a5df",
+    "url": "/lights-out/static/js/main.4f49a6ba.chunk.js"
+  },
+  {
+    "revision": "ca984014f05ee52b6d63",
+    "url": "/lights-out/static/js/runtime~main.5e0201cd.js"
+  }
+]);
